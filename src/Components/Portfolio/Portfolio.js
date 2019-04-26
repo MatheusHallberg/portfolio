7ad4ee3a1c68cb6971b782f0b4e699a3771fcab0
MatheusHallberg <42592597+MatheusHallberg@users.noreply.github.com>
2019-04-26 17:40:18 -0300

    Mudanças slide

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -1,11 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
+import Slider from "react-slick";
 
-const portfolio = () => {
-  return (
-    <div className="container-section">
-      <h1> portfolio </h1>
-    </div>
-  );
-};
-
-export default portfolio;
+export default class SimpleSlider extends Component {
+  render() {
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    };
+    return (
+      <div>
+        <h2> Single Item</h2>
+        <Slider {...settings}>
+          <div>
+            <h3>HTML5</h3>
+            <image />
+          </div>
+          <div>
+            <h3>CSS3</h3>
+            <image />
+          </div>
+          <div>
+            <h3>JS</h3>
+            <image />
+          </div>
+          <div>
+            <h3>4</h3>
+          </div>
+          <div>
+            <h3>5</h3>
+          </div>
+          <div>
+            <h3>6</h3>
+          </div>
+        </Slider>
+      </div>
+    );
+  }
+}
