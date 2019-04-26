b3cffe577f0b8051bba3da8abdb5b635497cb514
Matheus <42592597+MatheusHallberg@users.noreply.github.com>
2019-04-26 17:55:43 -0300

    About update

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -8,7 +8,12 @@ const about = () => {
       <div>
         <h1 className={AboutStyle.title}> Olá,</h1>
         <h3 className={AboutStyle.content}>
-          eu sou um estudante de TI me aventurando no mundo de programação.
+          eu sou um estudante de TI me aventurando no mundo de programação. Meu
+          primeiro interesse em programação foi quando acabei dando de cara em
+          uma parede invisível em um videogame e muitas perguntas se formaram em
+          minha cabeça. Quando não estou estudando ou tentando alguma coisa no VS Code
+          eu provavelmente vou estar jogando ou saindo com meus amigos, assistindo anime,
+          brincando com meus cachorros.
         </h3>
       </div>
     </div>
